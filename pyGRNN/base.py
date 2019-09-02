f10397045641e0cfcd06312ce5bb1ae9e663f601
Federico Amato <48997810+federhub@users.noreply.github.com>
2019-09-02 14:51:14 +0200

    Update base.py

--- pyGRNN/base.py
+++ pyGRNN/base.py
@@ -7,19 +7,21 @@ Created on Mon Feb 11 11:39:38 2019
 import itertools 
 
 def countX(lst, x):
-    '''A function counting the number of time an element is present in a list'''
+    '''An internal function counting the number of time an element is present in a list'''
     return lst.count(x) 
 
 def list_combs(x):
-    '''A function returning all the possible combination between n numbers'''
+    '''An internal function returning all the possible combination between n numbers'''
     return [c for i in range(len(x)+1) for c in itertools.combinations(x,i)]
 
 
 def combs(x):
+    '''An internal function chaining iterables'''
     return itertools.chain.from_iterable(
         itertools.combinations(x, i + 1)
         for i in range(len(x)+1))
 
 def recursive_combination_excluder(lst, r, excludes):
+    '''An internal function to recursively exclude combinations diring forward-backward feature selection'''
     return (comb for comb in itertools.combinations(lst, r)
                  if any(e.issubset(comb) for e in excludes))
