c7e42de4280d256cf920c909622856094abc1eb9
Federico Amato <48997810+federhub@users.noreply.github.com>
2019-10-01 10:48:11 +0200

    Update __init__.py

--- pyGRNN/__init__.py
+++ pyGRNN/__init__.py
@@ -4,5 +4,5 @@ Created on Fri Feb  8 15:03:06 2019
 
 @author: famato1
 """
-from GRNN.py import GRNN
-from feature_selection.py import Isotropic_selector, Anisotropic_selector
+from .GRNN import GRNN
+from .feature_selection import Isotropic_selector, Anisotropic_selector
