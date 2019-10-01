bd22ac95d51009ab2dfc0b2123cfc682103bf701
Federico Amato <48997810+federhub@users.noreply.github.com>
2019-10-01 10:23:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyGRNN',         
   packages = ['pyGRNN'],   
-  version = 'v0.0.2-beta',      
+  version = 'v0.0.2',      
   license='MIT',      
   description = 'Python implementation of General Regression Neural Network (Nadaraya-Watson Estimator). A Feature Selection module based on GRNN is also provided',   # Give a short description about your library
   author = 'Federico Amato',                   
   author_email = 'federico.amato@unil.ch',      #
   url = 'https://github.com/federhub/pyGRNN',  
-  download_url = 'https://github.com/federhub/pyGRNN/archive/v0.0.2-beta.tar.gz',    #
+  download_url = 'https://github.com/federhub/pyGRNN/archive/v0.0.2.tar.gz',    
   keywords = ['Machine Learning', 'General Regression Neural Network', 'Kernel Regression', 'Feature Selection'],   
   install_requires=[
           'pandas',
